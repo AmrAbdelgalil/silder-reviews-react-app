5f52da5418c5c49581ee618eccdb58400152d54b
AmrAbdelgalil <93317873+AmrAbdelgalil@users.noreply.github.com>
2022-05-17 09:57:13 +0200

    Method no.2 to build Autoplay Silder Project

--- src/App.js
+++ src/App.js
@@ -4,46 +4,24 @@ import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
-  const [autoSilder, setAutoSilder] = useState(true);
   const [value, setValue] = useState(0);
 
-  const { id, image, name, title, quote } = people[value];
-
-  function nextReview() {
-    setAutoSilder(false);
-    setValue((prevValue) => {
-      if (prevValue === people.length - 1) {
-        return 0;
-      }
-      return prevValue + 1;
-    });
-  }
-
-  function prevReview() {
-    setAutoSilder(false);
-    setValue((prevValue) => {
-      if (prevValue === 0) {
-        return people.length - 1;
-      }
-      return prevValue - 1;
-    });
-  }
-
-  function autoReview() {
-    setValue((prevValue) => {
-      if (prevValue === people.length - 1) {
-        return 0;
-      }
-      return prevValue + 1;
-    });
-  }
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (value < 0) {
+      setValue(lastIndex);
+    }
+    if (value > lastIndex) {
+      setValue(0);
+    }
+  }, [value, people]);
 
   useEffect(() => {
-    const auto = setTimeout(autoReview, 2000);
-    if (autoSilder === false) {
-      clearTimeout(auto);
-    }
-  });
+    const autoPLay = setInterval(() => {
+      setValue(value + 1);
+    }, 3000);
+    return () => clearInterval(autoPLay);
+  }, [value]);
 
   return (
     <section className="section">
@@ -53,17 +31,34 @@ function App() {
         </h2>
       </div>
       <div className="section-center">
-        <article>
-          <img src={image} alt={name} className="person-img" />
-          <h4>{name}</h4>
-          <h4 className="title">{title}</h4>
-          <p className="text">{quote}</p>
-          <FaQuoteRight className="icon" />
-        </article>
-        <button className="prev" onClick={prevReview}>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === value) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === value - 1 ||
+            (value === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <h4 className="title">{title}</h4>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setValue(value - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={nextReview}>
+        <button className="next" onClick={() => setValue(value + 1)}>
           <FiChevronRight />
         </button>
       </div>
